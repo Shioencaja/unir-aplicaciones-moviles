aa4fa3a3b5f19cd5d93e13ed35b1d5a41c295c93
Gabriel Ballón <88945451+Shioencaja@users.noreply.github.com>
2023-07-03 04:05:03 -0500

    cambio de props

--- src/pages/favoritos/index.tsx
+++ src/pages/favoritos/index.tsx
@@ -56,7 +56,7 @@ export default function Favoritos({ favorites }: Props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const favorites = await getItems();
 
   return {
--- src/pages/productos/index.tsx
+++ src/pages/productos/index.tsx
@@ -56,7 +56,7 @@ export default function Productos({ products }: Props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const results = await getProducts();
   return {
     props: {
